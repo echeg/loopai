SvetlovA/ralphex

fix: detect claude individual spend limit in default patterns (#426)
* fix: detect claude individual spend limit in default patterns

claude code emits "You've hit your individual spend limit · run /usage-credits
to raise it" on team plans when a member's extra-usage budget is exhausted.
"individual spend" sits between "your" and "limit", so the existing
"You've hit your limit" default cannot substring-match it. patternError
returned nil and the run died with the raw "claude exited with error:
command wait: exit status 1" instead of a limit diagnostic.

add the wording to both default lists, mirroring 3c461d39 and d70c8944:
claude_error_patterns for a graceful exit when --wait is not configured, and
claude_limit_patterns so wait_on_limit retries through the reset window.

the phrase is long and specific, so it does not reintroduce the narrated-prose
false positives fixed in #419 and #321.

Related to #425

* docs: name the real carriers of the pattern defaults in CLAUDE.md

CLAUDE.md said the claude_error_patterns / codex_error_patterns defaults live in
"llms.txt and the embedded config". llms.txt carries the limit-pattern defaults
only — the error-pattern list appears nowhere in it, and the file that does
mirror it is the README config table.

the line is the map a pattern addition follows, so the wrong pointer sends the
next contributor to a file that has nothing to update and leaves the README
table advertising a default list that no longer ships.

name the actual carriers on both lines: embedded config plus the README table,
and llms.txt for limit patterns only.

pkg/config/values_test.go
@@ -70,9 +70,9 @@ func TestValuesLoader_Load_EmbeddedOnly(t *testing.T) {
 	assert.Equal(t, "docs/plans", values.PlansDir)
 	assert.Equal(t, "git", values.VcsCommand)
 	assert.Empty(t, values.CommitTrailer)
-	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "API Error: 400", "API Error: 401", "API Error: 403", "API Error: 404", "API Error: 413", "API Error: 429", "API Error: 500", "cannot be launched inside another Claude Code session", "Not logged in", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit"}, values.ClaudeErrorPatterns)
+	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "API Error: 400", "API Error: 401", "API Error: 403", "API Error: 404", "API Error: 413", "API Error: 429", "API Error: 500", "cannot be launched inside another Claude Code session", "Not logged in", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You've hit your individual spend limit"}, values.ClaudeErrorPatterns)
 	assert.Equal(t, []string{"Rate limit exceeded", "rate limit reached", "429 Too Many Requests", "quota exceeded", "insufficient_quota", "You've hit your usage limit"}, values.CodexErrorPatterns)
-	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit"}, values.ClaudeLimitPatterns)
+	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You've hit your individual spend limit"}, values.ClaudeLimitPatterns)
 	assert.Equal(t, []string{"Rate limit exceeded", "rate limit reached", "429 Too Many Requests", "quota exceeded", "insufficient_quota", "You've hit your usage limit"}, values.CodexLimitPatterns)
 	assert.Equal(t, []string{"FYA_TRANSIENT_TIMEOUT", "API Error: 529", "API Error: 502", "API Error: 503", "API Error: 504"}, values.ClaudeRetryPatterns)
 	assert.Zero(t, values.WaitOnLimit)

echeg/loopai

(cherry picked from commit 407fe8f0674ff80fa433a56e0a4b6bf4e15ab4b3)

pkg/config/values_test.go
@@ -77,9 +77,9 @@ func TestValuesLoader_Load_EmbeddedOnly(t *testing.T) {
 	assert.Equal(t, "docs/plans", values.PlansDir)
 	assert.Equal(t, "git", values.VcsCommand)
 	assert.Empty(t, values.CommitTrailer)
-	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "API Error: 400", "API Error: 401", "API Error: 403", "API Error: 404", "API Error: 413", "API Error: 429", "API Error: 500", "cannot be launched inside another Claude Code session", "Not logged in", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You're out of usage credits"}, values.ClaudeErrorPatterns)
+	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "API Error: 400", "API Error: 401", "API Error: 403", "API Error: 404", "API Error: 413", "API Error: 429", "API Error: 500", "cannot be launched inside another Claude Code session", "Not logged in", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You're out of usage credits", "You've hit your individual spend limit"}, values.ClaudeErrorPatterns)
 	assert.Equal(t, []string{"Rate limit exceeded", "rate limit reached", "429 Too Many Requests", "quota exceeded", "insufficient_quota", "You've hit your usage limit"}, values.CodexErrorPatterns)
-	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You're out of usage credits"}, values.ClaudeLimitPatterns)
+	assert.Equal(t, []string{"You've hit your limit", "You've hit your session limit", "Your usage allocation has been disabled by your admin", "You've hit your org's monthly usage limit", "You're out of usage credits", "You've hit your individual spend limit"}, values.ClaudeLimitPatterns)
 	assert.Equal(t, []string{"Rate limit exceeded", "rate limit reached", "429 Too Many Requests", "quota exceeded", "insufficient_quota", "You've hit your usage limit", "is at capacity", "You hit your spend cap"}, values.CodexLimitPatterns)
 	assert.Equal(t, []string{"FYA_TRANSIENT_TIMEOUT", "API Error: 529", "API Error: 502", "API Error: 503", "API Error: 504"}, values.ClaudeRetryPatterns)
 	assert.Equal(t, 10*time.Minute, values.WaitOnLimit)